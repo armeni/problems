357951e43120f796636959fc5683be11eaed62a3
Armen Sardaryan <32457051+armeni@users.noreply.github.com>
2017-10-23 19:52:28 +0300

    Update problem_2.py

--- pack_3/problem_2.py
+++ pack_3/problem_2.py
@@ -1,28 +1,28 @@
 class converter(int):
+    lst = (('M', 1000),('CM', 900),('D', 500),('CD', 400),('C', 100),('XC', 90),('L', 50),('XL', 40),('X', 10),('IX', 9),('V', 5),('IV', 4),('I',1))
+    
     def roman2arab(self,s):
-        rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        ar = 0
-        for i in range(len(s)):
-            if i > 0 and rom[s[i]] > rom[s[i - 1]]:
-                ar += rom[s[i]] - 2 * rom[s[i - 1]]
-            else:
-                ar += rom[s[i]]
-        return ar
-
+        res = 0
+        i = 0
+        for m, n in self.lst:
+            while s[i:i + len(m)] == m:
+                res += n
+                i += len(m)
+        return res  
+	
     def arab2roman(self,s):
-        ar = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),(50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
-        rom = ''
-        while s > 0:
-            for i, r in ar:
-                while s >= i:
-                    rom += r
-                    s -= i
-        return rom
-
+        res = ""
+        for m, n in self.lst:
+            while s>=n:
+                res += m
+                s -= n
+        return res
+    
+convert=converter()
 s=input('Введите число: ')
 is_int=s.isnumeric()
 
 if is_int:
-    print('Это число римскими цифрами: ', converter().arab2roman(int(s)))
+    print('Это число римскими цифрами: ', convert.arab2roman(int(s)))
 else:
-    print('Это число арабскими цифрами: ', converter().roman2arab(s))
+    print('Это число арабскими цифрами: ', convert.roman2arab(s))
