590d4e65380953587ea29d19c6d0b09a4ffeefbe
Armen Sardaryan <32457051+armeni@users.noreply.github.com>
2017-10-28 12:36:35 +0300

    Update problem_3.py

--- pack_3/problem_3.py
+++ pack_3/problem_3.py
@@ -1,25 +1,34 @@
 import math
 
-class Circle():
-    def __init__(self, r):
-        self.r = r
+class Figure:
+    def S(self):
+        pass
 
+    def P(self):
+        pass
+
+class Circle(Figure):
+    def __init__(self,r):
+        self.r = r
     def S(self):
         return self.r**2*math.pi
-
     def P(self):
         return 2*self.r*math.pi
 
-class Rectangle():
-    def __init__(self, l, w):
-        self.l=l
-        self.w=w
-
+class Rectangle(Figure):
+    def __init__(self,l,w):
+        self.l = l
+        self.w = w
     def S(self):
         return self.l*self.w
 
-r=int(input('Введите радиус окружности: '))
-print('Площадь окружности: ', Circle(r).S())
-print('Длина окружности: ', Circle(r).P())
-l,w=map(int,input('Введите длину и ширину прямогольника: ').split())
-print('Площадь прямоугольника: ', Rectangle(l,w).S())
+str = str(input('Введите название фигуры (круг или прямоугольник): '))
+if str == 'Круг' or str == 'круг':
+	r = float(input('Введите радиус окружности: '))
+	print('Площадь окружности: ', Circle(r).S())
+	print('Длина окружности: ', Circle(r).P())
+elif str == 'Прямоугольник' or str == 'прямоугольник':
+	l, w = map(float,input('Введите длину и ширину прямогольника: ').split())
+	print('Площадь прямоугольника: ', Rectangle(l,w).S())
+else:
+	print('Ошибка ввода')
